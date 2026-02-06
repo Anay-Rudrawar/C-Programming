051ff70b91c060d32fa8b1b7d2687ab59528703a
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-02-06 06:43:47 +0000

    Fix grading logic and output formatting in assignment_5

--- Assignment/assignment_5.c
+++ Assignment/assignment_5.c
@@ -14,4 +14,23 @@ int main() {
     for(i = 0; i < 5; i++) {
         scanf("%d", &marks[i]);
         total += marks[i];
+
+        if(marks[i] < 40) {
+            printf("Student is Fail\n");
+            return 0;
+        }
 }
+    percentage = (total / 500.0) * 100;
+    printf("Total marks: %.2f\n", total);
+    printf("Percentage: %.2f%%\n", percentage);
+    if(percentage >= 75) {
+        printf("Grade: Distinction\n");
+    } else if(percentage >= 60) {
+        printf("Grade: First Division\n");
+    } else if(percentage >= 50) {
+        printf("Grade: Second Division\n");
+    } else if(percentage >= 40) {
+        printf("Grade: Third Division\n");
+    } else {
+        printf("Student is Fail\n");
+    }
